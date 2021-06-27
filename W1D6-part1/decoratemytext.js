ca1789bbf082e5ec27fa52818481a818b248a0bf
Yordanos Desta Tegegn <yordan.desta@gmail.com>
2021-06-27 16:50:40 -0500

    finished the second project

---
+++ W1D6-part1/decoratemytext.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+    let dec = document.getElementById("decoration");
+    let tArea = document.getElementById("textarea");
+
+    dec.onclick = function() {
+        setInterval(increseFont, 500);
+    }
+
+    let blingcb = document.getElementById("bling");
+
+    blingcb.onclick = function() {
+        if (blingcb.checked) {
+            tArea.classList.add("bling-text");
+        } else {
+            tArea.classList.remove("bling-text");
+        }
+    }
+
+    const increseFont = function() {
+        let fSize = parseInt(window.getComputedStyle(tArea).fontSize) + 2;
+        tArea.style.fontSize = fSize + "pt";
+    };
+
+
+    let pigl = document.getElementById("piglatin");
+
+    pigl.onclick = function() {
+        let wordsA = tArea.value.split(" ");
+        let newWord = [];
+        wordsA.map((elem) => {
+            if (elem[0].toLowerCase() != 'a' &&
+                elem[0].toLowerCase() != 'e' &&
+                elem[0].toLowerCase() != 'o' &&
+                elem[0].toLowerCase() != 'u') {
+
+                let w = elem.split('');
+
+                let temp = w[0];
+
+
+                w[0] = w[w.length - 1];
+                w[w.length - 1] = temp;
+
+
+                w.push("-ay");
+                newWord.push(w.join(''));
+            } else {
+                elem += "-ay";
+            }
+        });
+
+        tArea.value = newWord.join(" ");
+    }
+}
