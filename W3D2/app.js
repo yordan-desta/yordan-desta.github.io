b2296199c557473f4ecebc7c21c98527b6d389b7
Yordanos Desta Tegegn <yordan.desta@gmail.com>
2021-07-06 17:49:59 -0500

    finished jquery

---
+++ W3D2/app.js
@@ -0,0 +1,95 @@
+$(function() {
+
+    const colors = ["aqua", "chocolate", "blueviolet", "burlywood", "brown", "black", "cadetblue", "azure", "orange"];
+    let circleRad = 50;
+    let count = 0;
+    let rate = 10;
+    let speed = 250;
+
+
+    let timer = null;
+
+    $("#start").click(function() {
+        if ($("[name=width]").val()) {
+            circleRad = parseInt($("[name=width]").val());
+        }
+        if ($("[name=gamount]").val()) {
+            rate = parseInt($("[name=gamount]").val());
+        }
+        if ($("[name = gInterval]").val()) {
+            speed = $("[name=gInterval]").val();
+        }
+        if ($("[name = ncircle]").val()) {
+            count = $("[name=ncircle]").val();
+        }
+
+        start();
+    });
+
+    function makeCircles() {
+
+        const contWid = parseInt($("#circles").width());
+        let offset = 0;
+        let left = 0;
+
+        for (let i = 0; i < count; i++) {
+
+            if ((left + offset) > contWid) {
+                offset += 30;
+                left = 0;
+            }
+
+            left = left + offset;
+
+
+            const cr = $("<div>", {
+                "class": "circle"
+            });
+
+            cr.css({
+                "left": left + "px",
+                "background-color": getRandomColor(),
+                "border-radius": circleRad + "px",
+                "width": (circleRad * 2) + "px",
+                "height": (circleRad * 2) + "px"
+            });
+
+            cr.click(function() {
+                $(this).hide();
+            })
+
+            $("#circles").append(cr);
+
+            left += 50;
+        }
+    }
+
+    function start() {
+        stop();
+
+        $(".circle").remove();
+        makeCircles(10);
+        timer = setInterval(enlargeCircle, speed);
+    }
+
+    function stop() {
+        clearInterval(timer);
+    }
+
+    function enlargeCircle() {
+        const br = parseInt($(".circle").css("borderTopLeftRadius"));
+
+        $(".circle").css({
+            "height": (br * 2) + "px",
+            "width": (br * 2) + "px",
+            "border-radius": br + rate + "px",
+        });
+    }
+
+    function getRandomColor() {
+        const c = colors[Math.floor(Math.random() * colors.length)];
+        console.log(c);
+        return c;
+    }
+
+})
